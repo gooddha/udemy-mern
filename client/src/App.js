2b33b885cdd5b6fe6fac90efcb235625181a96ee
gooddha <gooddha@ya.ru>
2020-01-02 18:39:50 +0300

    Done Get Profile action

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,8 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 import './App.css';
 import {
   loadUser
@@ -33,43 +35,23 @@ const App = () => {
     store.dispatch(loadUser());
   }, []);
 
-  return ( <
-    Provider store = {
-      store
-    } >
-    <
-    Router >
-    <
-    Fragment >
-    <
-    Navbar / >
-    <
-    Route exact path = '/'
-    component = {
-      Landing
-    }
-    /> <
-    section className = 'container' >
-    <
-    Alert / >
-    <
-    Switch >
-    <
-    Route exact path = '/register'
-    component = {
-      Register
-    }
-    /> <
-    Route exact path = '/login'
-    component = {
-      Login
-    }
-    /> < /
-    Switch > <
-    /section> < /
-    Fragment > <
-    /Router> < /
-    Provider >
+  return ( 
+    <Provider store={ store }>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path = '/' component={Landing} /> 
+          <section className = 'container'>
+            <Alert />
+            <Switch>
+              <Route exact path = '/register' component = {Register} /> 
+              <Route exact path = '/login' component = {Login} /> 
+              <PrivateRoute exact path = '/dashboard' component = {Dashboard} /> 
+            </Switch> 
+          </section> 
+        </Fragment> 
+      </Router> 
+    </Provider >
   )
 };
 
