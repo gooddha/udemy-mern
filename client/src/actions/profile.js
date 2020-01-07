b3706c82f391cdbca39a6f99ffeb1d547573fc05
gooddha <gooddha@ya.ru>
2020-01-07 17:15:19 +0300

    Done account delete function at clientside

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -5,7 +5,9 @@ import { setAlert } from './alert';
 import {
   GET_PROFILE,
   UPDATE_PROFILE,
-  PROFILE_ERROR
+  PROFILE_ERROR,
+  CLEAR_PROFILE,
+  ACCOUNT_DELETED
 } from './types';
 
 // Get current user profile
@@ -122,3 +124,70 @@ export const addEducation = (formData, history) => async dispatch => {
     });   
   }
 };
+
+// Delete experience 
+export const deleteExperience = id => async dispatch => {
+  try {
+    const res = await axios.delete('/api/profile/experience/'+ id);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Experience Removed', 'success'));
+
+  } catch (err) {
+    
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statustext, status: err.response.status }
+    });    
+  }
+}
+
+// Delete education 
+export const deleteEducation = id => async dispatch => {
+  try {
+    const res = await axios.delete('/api/profile/education/'+ id);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Education Removed', 'success'));
+
+  } catch (err) {
+    
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statustext, status: err.response.status }
+    });    
+  }
+}
+
+// Delete Account and Profile
+export const deleteAccount = id => async dispatch => {
+  if (window.confirm('Are you shure? This can NOT be undone!')) {
+
+    
+    try {
+      const res = await axios.delete('/api/profile');
+      
+      dispatch({ type: CLEAR_PROFILE });
+      dispatch({ type: ACCOUNT_DELETED });
+      dispatch(setAlert('Your account has been permomently deleted'));
+
+    
+      dispatch(setAlert('Education Removed', 'success'));
+    
+    } catch (err) {
+      
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statustext, status: err.response.status }
+      });    
+    }
+  }
+}
