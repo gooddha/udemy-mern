4e59c0708693ad18fba8548d2f53cdbf1700f09b
Andrey Adamovich <gooddha@ya.ru>
2020-01-16 15:30:12 +0300

    Done post Like and Unlike functionality

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -5,32 +5,36 @@ import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
 import PostItem from './PostItem';
 
-const Posts = ({ getPosts, post: { posts, loading }}) => {
+const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
 
-  return loading ? <Spinner /> : <Fragment>
-    <h1 className="large text-primary">Posts</h1>
-    <p className="lead">
-      <i className="fas fa-user"></i> Welcome to the community
-    </p>
-    {/* */}
-    <div className="posts">
-      {posts.map(post => (
-        <PostItem key={post._id} post={post} />
-      ))}
-    </div>
-  </Fragment>
-}
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className='large text-primary'>Posts</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Welcome to the community
+      </p>
+      {/* */}
+      <div className='posts'>
+        {posts.map(post => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
+  );
+};
 
 Posts.propTypes = {
   getPosts: PropTypes.func.isRequired,
   post: PropTypes.object.isRequired
-}
+};
 
 const mapStateToProps = state => ({
   post: state.post
-})
+});
 
 export default connect(mapStateToProps, { getPosts })(Posts);
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,32 +1,38 @@
-import {
-  GET_POSTS,
-  POST_ERROR
-} from '../actions/types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from '../actions/types';
 
 const initialState = {
   posts: [],
   post: null,
   loading: true,
   error: {}
-}
+};
 
 export default function(state = initialState, action) {
   const { type, payload } = action;
 
-  switch(type) {
+  switch (type) {
     case GET_POSTS:
       return {
         ...state,
         posts: payload,
         loading: false
-      }
+      };
     case POST_ERROR:
       return {
         ...state,
         error: payload,
         loading: false
-      }
+      };
+    case UPDATE_LIKES:
+      console.log(payload);
+      return {
+        ...state,
+        posts: state.posts.map(post =>
+          post._id === payload.postId ? { ...post, likes: payload.likes } : post
+        ),
+        loading: false
+      };
     default:
-      return state
+      return state;
   }
 }
